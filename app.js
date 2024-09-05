34c4de40fb6eb99b7386814988ab03a07f244880
Jimmy Kim <jimmykim925@gmail.com>
2024-09-05 01:06:51 -0700

    folder

--- app.js
+++ app.js
@@ -9,7 +9,10 @@ const app = express();
 app.use(bodyParser.json());
 
 // Connect to MongoDB using the environment variable
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 // Check the connection
 const db = mongoose.connection;
