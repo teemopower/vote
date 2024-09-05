f78377245f5e6cb6539d946dac378538ac7d1d42
Jimmy Kim <jimmykim925@gmail.com>
2024-09-04 23:50:26 -0700

    updates

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const express = require('express');
 const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 require('dotenv').config();  // Load environment variables
 
 const app = express();
